2b0eec65b64709191f44c4ebbf2dba2591bb125c
nandkumar  birgad <nandkumarbirgad5@gmail.com>
2026-01-11 20:06:22 +0000

    Conditional

---
+++ java/Variable/Main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Main{
+    public static void main(String[] arg){
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        //variable 
+        // String name  = "Nandu";
+        // int age = 45;
+        // double price = 34.44;
+        // int a = 48;
+        // int b = 89;
+        // int c = a * b;
+        // System.out.println(c);
+
+
+        //Aritmatic
+
+        // int a = 20;
+        // int b = 56;
+        // int sum = a + b;
+        // int mul = a * b;
+        // int diff = a / b;
+        // System.out.println(b);
+
+
+        //input 
+
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // //nextINT
+        // System.out.println(name);
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // int a = 20;
+        // int b = 46;
+         int sum = a + b;
+        System.out.println(sum);
+
+    }
+}
