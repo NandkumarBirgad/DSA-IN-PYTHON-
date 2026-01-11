ffad85b0e4448af0fbaaeabc52c177deba3ee522
nandkumar  birgad <nandkumarbirgad5@gmail.com>
2026-01-11 19:48:08 +0000

    variable

--- Variable/Main.java
+++ Variable/Main.java
@@ -1,9 +1,18 @@
 class Main{
     public static void main(String[] arg){
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        //variable 
+        String name  = "Nandu";
+        int age = 45;
+        double price = 34.44;
+        int a = 48;
+        int b = 89;
+        int c = a * b;
+        System.out.println(c);
 
     }
 }
