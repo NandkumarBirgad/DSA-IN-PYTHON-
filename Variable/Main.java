05442264f3b522cbfeb797c3aeadfba337a096eb
nandkumar  birgad <nandkumarbirgad5@gmail.com>
2026-01-11 19:41:48 +0000

    add

---
+++ Variable/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] arg){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+    }
+}
