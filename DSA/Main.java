77fef7957e877ba23f277b77a1c9809a8abecb49
nandkumar  birgad <nandkumarbirgad5@gmail.com>
2026-01-19 11:13:59 +0000

    jj

---
+++ DSA/Main.java
@@ -0,0 +1,13 @@
+package DSA;
+
+public class Main {
+    int a = 2;
+    int count = 0;
+    int sum = 1;
+    while( count <= 1){
+        sum = sum + count;
+        count = count + 1;
+    }
+    System.out.println(sum);
+
+}
